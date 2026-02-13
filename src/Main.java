bbb5689e5ea65e73409c0b83cd8828cb90b8cb28
MG <gmelina69@gmail.com>
2026-02-13 11:22:57 +0400

    Update 2nd task

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,48 @@
 public class Main {
     public static void main(String[] args) {
 
-        int accountBalance = 500; // сумма на балансе клиента
-        int accountIncrease = 1899; // сумма пополнения счета
-            if (accountIncrease >= 1100) {
-                System.out.println("Вам начислено " + accountIncrease / 100 + " бонусa(ов)");
-                System.out.println("Баланс вашего счета равен: " + (accountBalance + accountIncrease + accountIncrease / 100) + " рубля(ей)");
-            } else {
-                System.out.println("Баланс вашего счета равен: " + (accountBalance + accountIncrease) + " рубля(ей)");
-                System.out.println("Вам начислено 0 бонусов");
-            }
+        int accountBalance = 600; // сумма на балансе клиента
+        int accountIncrease; // сумма пополнения счета
 
-            }
+        // Тест 1:
+        accountIncrease = 1099;
+        if (accountIncrease >= 1100) {
+            System.out.println("Вам начислено " + accountIncrease / 100 + " бонусa(ов)");
+            System.out.println("Баланс вашего счета равен: " + (accountBalance + accountIncrease + accountIncrease / 100) + " рубля(ей)");
+        } else {
+            System.out.println("Вам начислено 0 бонусов");
+            System.out.println("Баланс вашего счета равен: " + (accountBalance + accountIncrease) + " рубля(ей)");
+        }
+
+        // Тест 2:
+        accountIncrease = 1100;
+        if (accountIncrease >= 1100) {
+            System.out.println("Вам начислено " + accountIncrease / 100 + " бонусa(ов)");
+            System.out.println("Баланс вашего счета равен: " + (accountBalance + accountIncrease + accountIncrease / 100) + " рубля(ей)");
+        } else {
+            System.out.println("Вам начислено 0 бонусов");
+            System.out.println("Баланс вашего счета равен: " + (accountBalance + accountIncrease) + " рубля(ей)");
+        }
+
+        // Тест 3:
+        accountIncrease = 1199;
+        if (accountIncrease >= 1100) {
+            System.out.println("Вам начислено " + accountIncrease / 100 + " бонусa(ов)");
+            System.out.println("Баланс вашего счета равен: " + (accountBalance + accountIncrease + accountIncrease / 100) + " рубля(ей)");
+        } else {
+            System.out.println("Вам начислено 0 бонусов");
+            System.out.println("Баланс вашего счета равен: " + (accountBalance + accountIncrease) + " рубля(ей)");
+        }
+
+        // Тест 4:
+        accountIncrease = 1200;
+        if (accountIncrease >= 1100) {
+            System.out.println("Вам начислено " + accountIncrease / 100 + " бонусa(ов)");
+            System.out.println("Баланс вашего счета равен: " + (accountBalance + accountIncrease + accountIncrease / 100) + " рубля(ей)");
+        } else {
+            System.out.println("Вам начислено 0 бонусов");
+            System.out.println("Баланс вашего счета равен: " + (accountBalance + accountIncrease) + " рубля(ей)");
+        }
+
     }
+}
